29f8ded117cf6ff15534f0648efab7ce79dc888d
reginaenzner <enznerregina@gmail.com>
2026-01-08 14:50:28 +0100

    Implement Mqtt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,14 +5,12 @@ plugins {
 
 android {
     namespace = "com.examplehjhk.moveon"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.examplehjhk.moveon"
         minSdk = 26
-        targetSdk = 36
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -29,30 +27,22 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 }
 
-configurations.all {
-    exclude(group = "com.google.guava", module = "listenablefuture")
-}
-
 dependencies {
+
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.compiler)
-
-    // Room Database
-    val roomVersion = "2.6.1"
-    implementation("androidx.room:room-runtime:$roomVersion")
-    implementation("androidx.room:room-ktx:$roomVersion")
-
-
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 
+    // HiveMQ MQTT Client
+    implementation("com.hivemq:hivemq-mqtt-client:1.3.0")
 }
--- app/src/main/java/com/examplehjhk/moveon/MainActivity.java
+++ app/src/main/java/com/examplehjhk/moveon/MainActivity.java
@@ -1,24 +1,105 @@
 package com.examplehjhk.moveon;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import com.examplehjhk.moveon.hardware.mqtt.SimpleMqttClient;
+import com.examplehjhk.moveon.hardware.mqtt.data.MqttMessage;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String MQTT_BROKER_URI = "tcp://broker.hivemq.com:1883";
+    private static final String DEVICE_DATA_TOPIC = "moveon/device/data";
+
+    private SimpleMqttClient mqttClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("settings", MODE_PRIVATE);
+        boolean isDarkMode = sharedPreferences.getBoolean("dark_mode", false);
+
+        if (isDarkMode) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
+
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        mqttClient = new SimpleMqttClient("broker.hivemq.com", 1883, UUID.randomUUID().toString());
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        connectToMqttBroker();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mqttClient.unsubscribe(DEVICE_DATA_TOPIC);
+        mqttClient.disconnect();
+    }
+
+    private void connectToMqttBroker() {
+        mqttClient.connect(new SimpleMqttClient.MqttConnection(this) {
+            @Override
+            public void onSuccess() {
+                Toast.makeText(MainActivity.this, "MQTT Connected", Toast.LENGTH_SHORT).show();
+                subscribeToDeviceData();
+            }
+
+            @Override
+            public void onError(Throwable error) {
+                Log.e("MQTT", "Connection failed", error);
+                Toast.makeText(MainActivity.this, "MQTT Connection Failed", Toast.LENGTH_SHORT).show();
+            }
         });
+    }
+
+    private void subscribeToDeviceData() {
+        mqttClient.subscribe(new SimpleMqttClient.MqttSubscription(this, DEVICE_DATA_TOPIC) {
+            @Override
+            public void onMessage(String topic, String payload) {
+                try {
+                    MqttMessage mqttMessage = deserializeMessage(payload);
+                    float armAngle = mqttMessage.getArmAngle();
+                    float sliderValue = mqttMessage.getSliderValue();
+
+                    // --- HIER IST DER WICHTIGSTE PUNKT ---
+                    // Implementieren Sie hier Ihre Spiellogik, die auf den neuen Winkel reagiert.
+                    // z.B. updateBirdPosition(armAngle);
+                    Log.d("MQTT_DATA", "Received Arm Angle: " + armAngle + ", Slider: " + sliderValue);
+
+                } catch (JSONException e) {
+                    Log.e("MQTT", "Failed to deserialize message", e);
+                }
+            }
+
+            @Override
+            public void onError(Throwable error) {
+                Log.e("MQTT", "Subscription failed", error);
+            }
+        });
+    }
+
+    private MqttMessage deserializeMessage(String json) throws JSONException {
+        JSONObject jMessage = new JSONObject(json);
+        float armAngle = (float) jMessage.getDouble("armAngle");
+        float sliderValue = (float) jMessage.getDouble("sliderValue");
+        return new MqttMessage(armAngle, sliderValue);
     }
 }
